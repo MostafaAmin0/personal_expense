f992b268658a11150d9fc2567ea8ea0e01dd28d1
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-09-22 08:29:56 +0200

    using Keys

--- lib/widget/expense_list.dart
+++ lib/widget/expense_list.dart
@@ -10,8 +10,8 @@ class ExpenseList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return transactions.list.isEmpty
-        ? LayoutBuilder(
+    if (transactions.list.isEmpty) {
+      return LayoutBuilder(
             builder: (ctx, constraints) {
               return Column(
                 children: [
@@ -24,21 +24,35 @@ class ExpenseList extends StatelessWidget {
                       'assets/image/waiting.png',
                       fit: BoxFit.cover,
                     ),
-                    height: constraints.maxHeight*0.6,
+                    height: constraints.maxHeight * 0.6,
                   ),
                 ],
               );
             },
-          )
-        : ListView.builder(
-            padding: EdgeInsets.all(10.0),
-            itemCount: transactions.list.length,
-            itemBuilder: (ctx, index) {
-              return TransItem(
-                tx: transactions.list[index],
-                deleteTrans: deleteTrans,
-              );
-            },
           );
+    } else {
+      return ListView(
+            padding: EdgeInsets.all(10.0),
+            children: transactions.list.map<Widget>(
+              (tx) => TransItem(
+                key: ValueKey(tx.id),
+                tx: tx,
+                deleteTrans: deleteTrans,
+              ),
+            ).toList(),
+          );
+    }
+
+    // ListView.builder(
+    //     padding: EdgeInsets.all(10.0),
+    //     itemCount: transactions.list.length,
+    //     itemBuilder: (ctx, index) {
+    //       return TransItem(
+    //         key: ValueKey( transactions.list[index].id),
+    //         tx: transactions.list[index],
+    //         deleteTrans: deleteTrans,
+    //       );
+    //     },
+    //   );
   }
 }
--- lib/widget/trans_item.dart
+++ lib/widget/trans_item.dart
@@ -1,12 +1,38 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personal_expense/models/transaction.dart';
 
-class TransItem extends StatelessWidget {
+class TransItem extends StatefulWidget {
   final Transaction tx;
   final Function deleteTrans;
 
-  TransItem({required this.tx, required this.deleteTrans});
+  const TransItem({
+    Key? key,
+    required this.tx,
+    required this.deleteTrans,
+  }) : super(key: key);
+
+  @override
+  State<TransItem> createState() => _TransItemState();
+}
+
+class _TransItemState extends State<TransItem> {
+  late Color _bgColor;
+
+  @override
+  void initState() {
+    super.initState();
+
+    const availableColors = [
+      Colors.blue,
+      Colors.purple,
+      Colors.teal,
+      Colors.black,
+    ];
+    _bgColor = availableColors[Random().nextInt(4)];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,20 +41,21 @@ class TransItem extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 8.0),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30.0,
           child: Padding(
             padding: const EdgeInsets.all(6.0),
             child: FittedBox(
-              child: Text('\$${tx.money.toStringAsFixed(2)}'),
+              child: Text('\$${widget.tx.money.toStringAsFixed(2)}'),
             ),
           ),
         ),
         title: Text(
-          tx.name,
+          widget.tx.name,
           style: Theme.of(context).textTheme.headline6,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(tx.date),
+          DateFormat.yMMMd().format(widget.tx.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
@@ -36,7 +63,7 @@ class TransItem extends StatelessWidget {
                 style:
                     TextButton.styleFrom(primary: Theme.of(context).errorColor),
                 onPressed: () {
-                  deleteTrans(tx.id);
+                  widget.deleteTrans(widget.tx.id);
                 },
                 label: Text(
                   'Delete',
@@ -48,58 +75,10 @@ class TransItem extends StatelessWidget {
                 style:
                     TextButton.styleFrom(primary: Theme.of(context).errorColor),
                 onPressed: () {
-                  deleteTrans(tx.id);
+                  widget.deleteTrans(widget.tx.id);
                 },
               ),
       ),
     );
   }
-
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          margin: EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 15,
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(width: 2.0, color: Theme.of(context).primaryColorDark),
-          ),
-          padding: EdgeInsets.all(2.5),
-          child: Text(
-            '\$${money.toStringAsFixed(2)}',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
-              color: Theme.of(context).primaryColorDark,
-            ),
-          ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              name,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
-              ),
-            ),
-            Text(
-              DateFormat.yMMMd().format(date),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 12.0,
-                color: Colors.grey,
-              ),
-            )
-          ],
-        )
-      ],
-    );
-  }
-  */
 }
